0df9d7fec389daead7aad934d4865c58a1056811
Bernscaiado <becaiado@gmail.com>
2022-11-15 16:42:38 -0500

    backup

--- app/src/main/java/com/example/profilemanager/MainActivity.java
+++ app/src/main/java/com/example/profilemanager/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.profilemanager;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,5 +45,55 @@ public class MainActivity extends AppCompatActivity {
         mapIntent.setPackage("com.google.android.apps.maps");
 
         startActivity(mapIntent);
+    }
+
+    ActivityResultLauncher<Intent> profileActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        ImageView avatarImage = (ImageView) findViewById(R.id.imageView);
+
+                        String drawableName = "flag_02";
+                        switch (data.getIntExtra("imageID", R.id.canada)) {
+                            case R.id.canada:
+                                drawableName = "flag_ca";
+                                break;
+                            case R.id.france:
+                                drawableName = "flag_fr";
+                                break;
+                            case R.id.iran:
+                                drawableName = "flag_eg";
+                                break;
+                            case R.id.spain:
+                                drawableName = "flag_sp";
+                                break;
+                            case R.id.england:
+                                drawableName = "flag_uk";
+                                break;
+                            case R.id.korea:
+                                drawableName = "flag_kr";
+                                break;
+                            case R.id.turkey:
+                                drawableName = "flag_tr";
+                                break;
+                            case R.id.japan:
+                                drawableName = "flag_jp";
+                                break;
+                            case R.id.usa:
+                                drawableName = "flag_us";
+                                break;
+                        }
+                        int resID = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+                        avatarImage.setImageResource(resID);
+                    }
+                }
+            });
+
+    public void OnSetAvatarButton(View v) {
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        profileActivityResultLauncher.launch(intent);
     }
 }
--- app/src/main/java/com/example/profilemanager/ProfileActivity.java
+++ app/src/main/java/com/example/profilemanager/ProfileActivity.java
@@ -2,7 +2,11 @@ package com.example.profilemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void SetTeamIcon(View v) {
+        Intent returnIntent = new Intent();
+
+        ImageView selectedImage = (ImageView) v;
+
+        returnIntent.putExtra("imageID", selectedImage.getId());
+
+        setResult(RESULT_OK, returnIntent);
+
+        finish();
     }
 }
