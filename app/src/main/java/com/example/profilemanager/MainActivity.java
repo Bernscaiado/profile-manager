ee959b5a7c6057cee4c7c79ecf1a3c3c642abf6d
Bernscaiado <becaiado@gmail.com>
2022-11-15 15:26:46 -0500

    added google maps feature

--- app/src/main/java/com/example/profilemanager/MainActivity.java
+++ app/src/main/java/com/example/profilemanager/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.profilemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OnOpenInGoogleMaps(v);
+            }
+        });
+
+    }
+
+
+    public void OnOpenInGoogleMaps(View v) {
+        EditText teamAddress = (EditText) findViewById(R.id.teamAddress);
+
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q=" + teamAddress.getText());
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        startActivity(mapIntent);
     }
 }
